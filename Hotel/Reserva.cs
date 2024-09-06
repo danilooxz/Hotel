d4732327eb1ac56ced584bae4a923244a0f83aa4
Danilo Marcon Júnior <danilo.2023014102@alunocesul.com>
2024-09-05 22:05:24 -0300

    Adicionar arquivos de projeto.

---
+++ Hotel/Reserva.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel
+{
+    public class Reserva
+    {
+        public DateTime DataCheckIn { get; set; }
+        public DateTime DataCheckOut { get; set; }
+
+        public int Quartos { get; set; }
+
+        public Reserva(DateTime dataCheckIn, DateTime dataCheckOut, int quartos)
+        {
+            DataCheckIn = dataCheckIn;
+            DataCheckOut = dataCheckOut;
+            Quartos = quartos;
+        }
+    }
+}
